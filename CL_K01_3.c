709b01c99dee0def5d4c52ca51e25e8ba59f4997
uchida eri <basstrplove@yahoo.co.jp>
2013-01-17 10:02:43 +0900

    応用ﾌﾟﾛｸﾞﾗﾐﾝｸﾞＡ課題１
    数字を変更

--- CL_K01_3.c
+++ CL_K01_3.c
@@ -7,7 +7,7 @@
 void main(void) {
 	double radius;
 
-	radius = 5.1 ;
+	radius = 5.111 ;
 
 	printf("”¼Œa%f‚Ì‰~ü‚Í%f‚Å‚·B\n",radius,radius*2*3.141593);
     printf("”¼Œa%f‚Ì‰~‚Ì–ÊÏ‚Í%f‚Å‚·B\n",radius,radius*radius*3.141593);
