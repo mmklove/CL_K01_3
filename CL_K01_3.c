381cca58842aba44f6d4b3adf6e563b4674701fb
uchida eri <basstrplove@yahoo.co.jp>
2013-01-17 09:56:58 +0900

    応用ﾌﾟﾛｸﾞﾗﾐﾝｸﾞＡの課題１
    アップ

---
+++ CL_K01_3.c
@@ -0,0 +1,14 @@
+/*ì¬Ò:1T3(3)@“à“cŒb—@*/
+/*‰Û‘è–¼:CL_K01_3 */
+
+#include <stdio.h>
+
+
+void main(void) {
+	double radius;
+
+	radius = 5.0 ;
+
+	printf("”¼Œa%f‚Ì‰~ü‚Í%f‚Å‚·B\n",radius,radius*2*3.141593);
+    printf("”¼Œa%f‚Ì‰~‚Ì–ÊÏ‚Í%f‚Å‚·B\n",radius,radius*radius*3.141593);
+}
