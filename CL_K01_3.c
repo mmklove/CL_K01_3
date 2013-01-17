30b51842e82b8bff4066f8f3d40e7c3d77bf1af3
uchida eri <basstrplove@yahoo.co.jp>
2013-01-17 10:03:25 +0900

    応用プラグラミングＡ課題１
    文字を変更

--- CL_K01_3.c
+++ CL_K01_3.c
@@ -7,8 +7,8 @@
 void main(void) {
 	double radius;
 
-	radius = 5.111 ;
+	radius = 5.0 ;
 
-	printf("”¼Œa%f‚Ì‰~ü‚Í%f‚Å‚·B\n",radius,radius*2*3.141593);
+	printf("”¼%f‚Ì‰~ü‚Í%f‚Å‚·B\n",radius,radius*2*3.141593);
     printf("”¼Œa%f‚Ì‰~‚Ì–ÊÏ‚Í%f‚Å‚·B\n",radius,radius*radius*3.141593);
 }
